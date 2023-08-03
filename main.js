e5d2b159ce31b217f8cde28db2b6e33cf9cee3f1
ShreyasJ1 <109956165+ShreyasJ1@users.noreply.github.com>
2023-08-03 10:44:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 img = "";
+status = "";
+
 
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - detecting objects";
 }
 
 function preload() {
@@ -14,7 +18,27 @@ function draw() {
     fill("red");
     text("dog", 45, 75);
     noFill();
-    stroke("black");
+    stroke("red");
     rect(30, 60, 450, 350);
+    fill("blue");
+    text("cat", 320, 120);
+    noFill();
+    stroke("blue");
+    rect(300, 90, 270, 320);
 
+}
+
+function modelLoaded() {
+    console.log("CocoSSD is initialized");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error,results) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+    }
 }
